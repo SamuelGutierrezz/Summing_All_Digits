f71643dbfa4848cab47e1adc36db6070504e82aa
SammyG <lumppath@gmail.com>
2023-09-30 13:58:00 -0700

    Committing -Add all digits- mini program

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int sum_of_digits(int n)
+{
+	int sum = 0;
+	for (int i = 1; i <= n; ++i)
+		sum += i;
+
+	return sum;
+}
+
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	int result = sum_of_digits(n);
+	cout << result << endl;
+
+	return 0;6
+}
